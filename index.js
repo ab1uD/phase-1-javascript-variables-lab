aa7c020e0626ac2034064043234548dc341613e1
ab1uD <abiud.nanjira@student.moringaschool.com>
2025-09-10 17:36:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+
+ // Declare the companyName using const
+const companyName = 'Scuber';
+  it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+  it('is defined as a const', function () {
+      expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+    });
+
+  it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+     it('is defined as a const', function () {
+      expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+    });
+
+    // Define mostProfitableNeighborhood using let
+    
+    expect(mostProfitableNeighborhood).to.equal('Chelsea');
+ it('is defined using let', function () {
+    expect(js).to.match(
+      "Expected mostProfitableNeighborhood to be defined using let")
+   });
+   describe('mostProfitableNeighborhood', function () {
+  it('is declared as equal to Chelsea', function () {
+    expect(mostProfitableNeighborhood).to.equal('Chelsea')});
+  });
+
+
+let companyCeo = 'Susan Smith';
+  describe('companyCeo', function () {
+  it('is declared as equal to Susan Smith', function () {
+    expect(companyCeo).to.equal('Susan Smith');
+  })});
+  it('is defined using let', function () {
+    expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+  });
